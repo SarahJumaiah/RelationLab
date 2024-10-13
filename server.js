761e2d1c2e58cb3306d1e901e00da206be564149
sarahjumaiah <sarahjumaiah@gmail.com>
2024-10-13 15:15:05 +0300

    done

--- server.js
+++ server.js
@@ -1,81 +1,21 @@
 import express from 'express';
 import mongoose from 'mongoose';
-import Book from './models/book.js';
 import dotenv from 'dotenv';
+import articleRouter from './routes/articleRouter.js';
+import blogRouter from './routes/blogRouter.js';
 
 dotenv.config();
 
 const app = express();
-const port = 1423;
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
+app.use('/api', articleRouter);
+app.use('/api', blogRouter);
 
-function main() {
-    mongoose.connect(process.env.MONGO_URL)
-        .then(() => {
-            console.log("Connected");
-        })
-        .catch((error) => {
-            console.error("Error:", error);
-        });
-}
-
-main();
-
-app.post('/books', (req, res) => {
-    const newBook = new Book({
-        title: req.body.title,
-        author: req.body.author,
-        editionNumber: req.body.editionNumber,
-        publicationDate: req.body.publicationDate,
-        hasEbook: req.body.hasEbook,
-        price: req.body.price,
-        supportedLanguages: req.body.supportedLanguages,
-        category: req.body.category
-    });
-
-    newBook.save()
-        .then(() => res.send('done'))
-        .catch((error) => res.send('error'));
-});
-
-app.get('/books', (req, res) => {
-    Book.find()
-        .then((books) => res.send(books))
-        .catch(() => res.send('error'));
-});
-
-app.get('/book/:id', (req, res) => {
-    const { id } = req.params;
-    Book.findById(id)
-        .then((book) => res.send(book))
-        .catch(() => res.send('error'));
-});
-
-app.patch('/book/:id', (req, res) => {
-    const { id } = req.params;
-    const updatedBook = {
-        title: req.body.title,
-        author: req.body.author,
-        editionNumber: req.body.editionNumber,
-        publicationDate: req.body.publicationDate,
-        hasEbook: req.body.hasEbook,
-        price: req.body.price,
-        supportedLanguages: req.body.supportedLanguages,
-        category: req.body.category
-    };
-
-    Book.findByIdAndUpdate(id, updatedBook, { new: true })
-        .then(() => res.send('done'))
-        .catch(() => res.send('error'));
-});
-
-app.delete('/book/:id', (req, res) => {
-    const { id } = req.params;
-    Book.findByIdAndDelete(id)
-        .then(() => res.send('done'))
-        .catch(() => res.send('error'));
-});
+mongoose.connect(process.env.MONGO_URL)
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log('Error:', err));
 
 app.listen(port, () => {
     console.log(`Server running on http://localhost:${port}`);
